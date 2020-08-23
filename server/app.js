87bd6930d6fcffb7f32a598abbe3bc3292ae3c5f
adnkamil <adnankamil27@gmail.com>
2020-08-23 13:10:38 +0800

    v11111

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV == 'development') {
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const route = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 const cors = require('cors')
